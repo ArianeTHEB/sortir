f6e6b2a6d4ef283597a40d91c4f021eb479a1129
ArianeThe <ariane.thebault2022@campus-eni.fr>
2022-08-22 18:30:05 +0200

    Ebauche d'architecture et premiers fichiers pour le site

---
+++ src/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends AbstractController
+
+{
+    /**
+     * @Route ("/", name="main_connexion")
+     */
+    public function home(): Response
+    {
+        return $this->render('main/connexion.html.twig');
+    }
+}
