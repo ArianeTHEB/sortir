005dc0f2f33c9b3847665246b654d262ea0cda95
seb_d <sebastien.duhamel2022@campus-eni.fr>
2022-08-23 14:33:53 +0200

    mise en place de la relation d'entité Sortie et Etat, modification nom de variable dans class

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -32,6 +32,10 @@ class Sortie
     #[ORM\Column(type: Types::TEXT)]
     private ?string $infosSortie = null;
 
+    #[ORM\ManyToOne(inversedBy: 'sorties')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Etat $etat = null;
+
         public function getId(): ?int
     {
         return $this->id;
@@ -105,6 +109,18 @@ class Sortie
     public function setInfosSortie(string $infosSortie): self
     {
         $this->infosSortie = $infosSortie;
+
+        return $this;
+    }
+
+    public function getEtat(): ?Etat
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(?Etat $etat): self
+    {
+        $this->etat = $etat;
 
         return $this;
     }
