0d919a95a68c08fe518ca61cb60f60e96b2aa736
seb_d <sebastien.duhamel2022@campus-eni.fr>
2022-08-25 11:11:19 +0200

    pré-formulaire création sortie, modification entité Sortie

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
-use App\Repository\CampusRepository;
+use App\Entity\Sortie;
+use App\Form\CreationSortieFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortiesController extends AbstractController
 {
-    #[Route('/sorties', name: 'sorties_list')]
-    public function list(CampusRepository $campusRepository): Response
+    #[Route('/creer', name: 'creer_sorties')]
+    public function ajouter(Request $request, EntityManagerInterface $entityManager): Response
+
     {
-        $var = "var";
-        return $this->render('sorties/list.html.twig',['var'=>$var]);
+        //création d'une instance de serie
+        $sortie = new Sortie();
+       // $sortie->setDateCreated(new \DateTime());//attribut nécessaire pour envoi bdd mais retiré du form
+        $sortieForm = $this -> createForm(CreationSortieFormType::class, $sortie);
+
+        dump($sortie);//permet de verifier si un objet est hydraté
+        $sortieForm -> handleRequest($request);
+        dump($sortie);//on voit a present que mon objet serie à des arguments grace à handleRequest
+
+        if($sortieForm->isSubmitted() && $sortieForm->isValid()){
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            //on crée un message flash pour signaler à l'utilisateur
+            $this->addFlash('success', 'Serie added! Good job.');
+
+            // on va à présent rediriger pour cela on utilise return
+            return $this->redirectToRoute('main_list');
+        }
+
+        //passage à twig pour déclencher l'affichage du formulaire
+        return $this->render('sorties/creerSortie.html.twig', [
+            'sortieForm' => $sortieForm ->createView()
+        ]);
     }
 }
--- src/Form/CreationSortieFormType.php
+++ src/Form/CreationSortieFormType.php
@@ -7,16 +7,21 @@ use App\Entity\Sortie;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ListeSortiesFormType extends AbstractType
+class CreationSortieFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionsMax')
+            ->add('duree')
+            ->add('infosSortie')
             ->add('campus',EntityType::class,[
                 'class' => Campus::class,
                 'choice_label' => 'nom',
@@ -25,17 +30,14 @@ class ListeSortiesFormType extends AbstractType
                         ->orderBy('c.nom','ASC');
                 }
             ])
-            ->add('contient',TextType::class,['mapped' =>false,'label' =>'Le nom de la sortie contient '])
-            ->add('entre',DateType::class, ['mapped' =>false,'html5' => true])
-            ->add('et',DateType::class, ['mapped' =>false,'html5' => true])
+            ->add('lieu')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
-
+            'data_class' => Sortie::class,
         ]);
     }
 }
