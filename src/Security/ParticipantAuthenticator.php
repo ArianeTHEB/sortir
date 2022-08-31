771e6eeee89ec3e00c8dcb1fe9cc3a1d267d48b2
ArianeThe <ariane.thebault2022@campus-eni.fr>
2022-08-31 10:08:15 +0200

    "se souvenir de moi" activé, avec cookie samesite en "strict" et durée de vie d'une semaine

--- src/Security/ParticipantAuthenticator.php
+++ src/Security/ParticipantAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -33,11 +34,20 @@ class ParticipantAuthenticator extends AbstractLoginFormAuthenticator
 
         return new Passport(
             new UserBadge($email),
-            new PasswordCredentials($request->request->get('password', '')),
+            new PasswordCredentials($request->request->get('password')),
             [
-                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
+                new RememberMeBadge(),
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token'))
             ]
         );
+ //       return new Passport(
+ //           new UserBadge($email),
+ //           new PasswordCredentials($request->request->get('password', '')),
+ //           [
+ //               ,
+ //           ]
+ //       );
+
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
